3974904fb867860d8a5b3cd866e6bd8152bb89f5
Eric Caron <eric.caron@gmail.com>
2020-03-17 20:53:51 -0500

    Overhaul to execute at form submission (#2)
    Core changes:
    * Removed older Caldera code they no longer have in mainline
    * Made it clearer when using v2 or v3
    * Passing WordPress CodeSniffer rules
    * Script now injects at footer of page
    * Execution only happens at form submission
    * v3 implementation is less likely to make Google think we want a v2

--- caldera-forms-anti-spam.php
+++ caldera-forms-anti-spam.php
@@ -3,16 +3,14 @@
 * Plugin Name: Caldera Forms Anti Spam
 * Description: Anti-spam for Caldera Forms. Recaptcha field!
 * Author: Yoohoo Plugins
-* Version: 0.3
+* Version: 0.4
 * Author URI: https://yoohooplugins.com
 * Text Domain: cf-anti-spam
  */
 
-define( 'CF_ANTISPAM_PATH',  plugin_dir_path( __FILE__ ) );
+define( 'CF_ANTISPAM_PATH', plugin_dir_path( __FILE__ ) );
 define( 'CF_ANTISPAM_URL',  plugin_dir_url( __FILE__ ) );
-define( 'CF_ANTISPAM_VER', '0.3' );
-
-
+define( 'CF_ANTISPAM_VER', '0.4' );
 
 add_action( 'caldera_forms_includes_complete', 'cf_antispam_init' );
 
@@ -20,7 +18,6 @@ function cf_antispam_init(){
 	Caldera_Forms_Autoloader::add_root( 'CF_Antispam', __DIR__ . '/classes' );
 	cf_antispam_init_recpatcha();
 }
-
 
 function cf_antispam_init_recpatcha(){
 	$recaptcha = new CF_Antispam_Recapatcha();
@@ -33,5 +30,5 @@ function cf_antispam_init_recpatcha(){
 
 	add_filter( 'caldera_forms_field_attributes-recaptcha', array( $recaptcha, 'field_attrs' ), 10, 2 );
 
-    add_filter( 'caldera_forms_summary_magic_fields', array( $recaptcha, 'remove_from_summary' ), 10, 2 );
+	add_filter( 'caldera_forms_summary_magic_fields', array( $recaptcha, 'remove_from_summary' ), 10, 2 );
 }
