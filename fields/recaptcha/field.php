c2da304c8998fa1e2ecd707886ece5c72d4906b6
Andrew Lima <andrew@arctek.co.za>
2020-09-28 17:55:49 +0200

    Revert "Merge pull request #19 from martin-braun/patch-1"
    This reverts commit c19581f34c5dcec2e3131f0784a784d542c960ba, reversing
    changes made to a7c35f3d1ecf56c6c3d0ad592a1ca52dacae228a.

--- fields/recaptcha/field.php
+++ fields/recaptcha/field.php
@@ -30,7 +30,14 @@ if (!empty($field['config']['recapv']) && $field['config']['recapv'] === 1 ) {
 
 <div id="cap<?php echo $field_id; ?>" class="g-recaptcha" data-sitekey="<?php echo $field['config']['public_key']; ?>" <?php if (!empty($field['config']['invis']) && $field['config']['invis'] === 1 ) { ?>data-size="invisible" <?php } ?>></div>
 
-<img onload="cf_load_recaptcha_<?php echo $field_id; ?>()" style="position:fixed;top:0;left:0;" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" />
+<script>
+	if(!window.cf_anti_loading_recaptcha) {
+		window.cf_anti_loading_recaptcha = true;
+		jQuery('.cf_anti_capfld').html('');
+		jQuery('script[src="<?php echo $script_url ?>"]').remove();
+		jQuery('<script>').attr('src', "<?php echo $script_url ?>").appendTo('body');
+	}
+</script>
 
 <?php echo $field_caption; ?>
 
@@ -44,39 +51,21 @@ if (!empty($field['config']['recapv']) && $field['config']['recapv'] === 1 ) {
 	<script>
 
 		var cf_recaptcha_is_ready = function (){
-			window.cf_anti_recaptcha_loading = false;
-			window.cf_anti_recaptcha_load = true;
+			window.cf_anti_loading_recaptcha = false;
 			jQuery(document).trigger("cf-anti-init-recaptcha");
 		}
 
-var cf_load_recaptcha_<?php echo $field_id; ?> = function() {
-	if(!window.cf_anti_recaptcha_load) {
-		jQuery(document).on("cf-anti-init-recaptcha", function(){
-			jQuery(document).on('click', '.reset_<?php echo $field_id; ?>', function(e){
-				e.preventDefault();
-				cf_init_recaptcha_<?php echo $field_id; ?>();
-			});
-			cf_init_recaptcha_<?php echo $field_id; ?>();
-		});
-		if(!window.cf_anti_recaptcha_loading) {
-			jQuery('<script>').attr('src', "<?php echo $script_url ?>").appendTo('body');
-			window.cf_anti_recaptcha_loading = true;
-		}
-	}
-	else {
-		jQuery('#cap<?php echo $field_id; ?>').html('');
-		cf_init_recaptcha_<?php echo $field_id; ?>();
-	}
-}
+		jQuery( function($){
 
-var cf_init_recaptcha_<?php echo $field_id; ?> = function() {
+			jQuery(document).on("cf-anti-init-recaptcha", function(){
+				function init_recaptcha_<?php echo $field_id; ?>(){
 
-					var captch = jQuery('#cap<?php echo $field_id; ?>');
+					var captch = $('#cap<?php echo $field_id; ?>').addClass('cf_anti_capfld');
 					<?php if (!empty($field['config']['recapv']) && $field['config']['recapv'] === 1 ) { ?>
 						grecaptcha.ready(function() {
-						var captch = jQuery('#cap<?php echo $field_id; ?>');
+							var captch = $('#cap<?php echo $field_id; ?>');
 					      	grecaptcha.execute("<?php echo trim( $field['config']['public_key'] ); ?>", {action: 'homepage'}).then(function(token) {
-					          jQuery('<input type="hidden" name="cf-recapv-token" value="' + token + '">').insertAfter(captch[0]);
+					          $('<input type="hidden" name="cf-recapv-token" value="' + token + '">').insertAfter(captch[0]);
 					      	});;
 					  	});
 					<?php } else { ?>
@@ -90,6 +79,15 @@ var cf_init_recaptcha_<?php echo $field_id; ?> = function() {
 						<?php if ( !empty( $field['config']['invis']) && $field['config']['invis'] === 1 ) { ?> grecaptcha.execute(); <?php } ?>
 					<?php } ?>
 				}
+
+				jQuery(document).on('click', '.reset_<?php echo $field_id; ?>', function(e){
+					e.preventDefault();
+					init_recaptcha_<?php echo $field_id; ?>();
+				});
+
+				init_recaptcha_<?php echo $field_id; ?>();
+			});
+		});
 
 	</script>
 
