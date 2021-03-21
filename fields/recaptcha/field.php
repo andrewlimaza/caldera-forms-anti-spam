9ea3e9cc17a986a9b263ba37be1324031545079b
Jarryd Long <JarrydLong@users.noreply.github.com>
2021-03-21 21:37:23 +0200

    Fixes the reCAPTCHA V2 On Load

--- fields/recaptcha/field.php
+++ fields/recaptcha/field.php
@@ -1,4 +1,12 @@
-<?php echo $wrapper_before; ?>
+<?php
+/**
+ * Renders the field in the visitor-facing form
+ *
+ * @package caldera-forms-anti-spam
+ */
+
+echo $wrapper_before; ?>
+
 <?php echo empty( $field['config']['recapv'] ) ? $field_label : ''; ?>
 
 <?php
@@ -21,6 +29,7 @@ $script_url = "https://www.google.com/recaptcha/api.js?onload=cf_recaptcha_is_re
 if (!empty($field['config']['recapv']) && $field['config']['recapv'] === 1 ) {
 	$script_url = "https://www.google.com/recaptcha/api.js?onload=cf_recaptcha_is_ready&render=" . trim($field['config']['public_key']) . "&hl=" . $language;
 }
+wp_enqueue_script('cf-anti-spam-recapthca-lib', $script_url);
 
 ?>
 
@@ -28,16 +37,11 @@ if (!empty($field['config']['recapv']) && $field['config']['recapv'] === 1 ) {
 
 <?php echo Caldera_Forms_Field_Input::html( $field, $field_structure, $form ); ?>
 
-<div id="cap<?php echo $field_id; ?>" class="g-recaptcha" data-sitekey="<?php echo $field['config']['public_key']; ?>" <?php if (!empty($field['config']['invis']) && $field['config']['invis'] === 1 ) { ?>data-size="invisible" <?php } ?>></div>
-
-<script>
-	if(!window.cf_anti_loading_recaptcha) {
-		window.cf_anti_loading_recaptcha = true;
-		jQuery('.cf_anti_capfld').html('');
-		jQuery('script[src="<?php echo $script_url ?>"]').remove();
-		jQuery('<script>').attr('src', "<?php echo $script_url ?>").appendTo('body');
-	}
-</script>
+<?php if (!empty($field['config']['recapv']) && $field['config']['recapv'] === 1 ) { ?>
+	<div id="cap<?php echo $field_id; ?>"></div>
+<?php } else { ?>
+	<div id="cap<?php echo $field_id; ?>" class="g-recaptcha" data-sitekey="<?php echo $field['config']['public_key']; ?>" <?php if (!empty($field['config']['invis']) && $field['config']['invis'] === 1 ) { ?>data-size="invisible" <?php } ?>></div>
+<?php } ?>
 
 <?php echo $field_caption; ?>
 
@@ -48,51 +52,44 @@ if (!empty($field['config']['recapv']) && $field['config']['recapv'] === 1 ) {
 	ob_start();
 ?>
 
-	<script>
+<script>
 
-		var cf_recaptcha_is_ready = function (){
-			window.cf_anti_loading_recaptcha = false;
-			jQuery(document).trigger("cf-anti-init-recaptcha");
-		}
+	var cf_recaptcha_is_ready = function (){
+		jQuery(document).trigger("cf-anti-init-recaptcha");
+	}
 
-		jQuery( function($){
+	jQuery( function($){
+        function init_recaptcha_<?php echo $field_id; ?>(){
 
-			jQuery(document).on("cf-anti-init-recaptcha", function(){
-				function init_recaptcha_<?php echo $field_id; ?>(){
+            var captch = jQuery('#cap<?php echo $field_id; ?>');
+            <?php if (!empty($field['config']['recapv']) && $field['config']['recapv'] === 1 ) { ?>
+            grecaptcha.ready(function() {
+                var captch = jQuery('#cap<?php echo $field_id; ?>');
+                grecaptcha.execute("<?php echo trim( $field['config']['public_key'] ); ?>", {action: 'homepage'}).then(function(token) {
+                    jQuery('<input type="hidden" name="cf-recapv-token" value="' + token + '">').insertAfter(captch[0]);
+                });
+            });
+            <?php } else { ?>
 
-					var captch = $('#cap<?php echo $field_id; ?>').addClass('cf_anti_capfld');
-					<?php if (!empty($field['config']['recapv']) && $field['config']['recapv'] === 1 ) { ?>
-						grecaptcha.ready(function() {
-							var captch = $('#cap<?php echo $field_id; ?>');
-					      	grecaptcha.execute("<?php echo trim( $field['config']['public_key'] ); ?>", {action: 'homepage'}).then(function(token) {
-					          $('<input type="hidden" name="cf-recapv-token" value="' + token + '">').insertAfter(captch[0]);
-					      	});;
-					  	});
-					<?php } else { ?>
-
-						captch.empty();
+            captch.empty();
 
 
-						grecaptcha.render( captch[0], { "sitekey" : "<?php echo trim( $field['config']['public_key'] ); ?>", "theme" : "<?php echo isset( $field['config']['theme'] ) ? $field['config']['theme'] : "light"; ?>" } );
+            grecaptcha.render( captch[0], { "sitekey" : "<?php echo trim( $field['config']['public_key'] ); ?>", "theme" : "<?php echo isset( $field['config']['theme'] ) ? $field['config']['theme'] : "light"; ?>" } );
 
-						// Only load grecaptcha.execute if it's set to invisible mode.
-						<?php if ( !empty( $field['config']['invis']) && $field['config']['invis'] === 1 ) { ?> grecaptcha.execute(); <?php } ?>
-					<?php } ?>
-				}
+            // Only load grecaptcha.execute if it's set to invisible mode.
+            <?php if ( !empty( $field['config']['invis']) && $field['config']['invis'] === 1 ) { ?> grecaptcha.execute(); <?php } ?>
+            <?php } ?>
+        }
 
-				jQuery(document).on('click', '.reset_<?php echo $field_id; ?>', function(e){
-					e.preventDefault();
-					init_recaptcha_<?php echo $field_id; ?>();
-				});
-
-				init_recaptcha_<?php echo $field_id; ?>();
-			});
+		jQuery(document).on("cf-anti-init-recaptcha", function(){
+			setTimeout( init_recaptcha_<?php echo $field_id; ?>(), 1000 );
 		});
+	});
 
-	</script>
+</script>
 
-	<?php
+<?php
 
-		$script_template = ob_get_clean();
+	$script_template = ob_get_clean();
 
-		Caldera_Forms_Render_Util::add_inline_data( $script_template, $form );
+	Caldera_Forms_Render_Util::add_inline_data( $script_template, $form );
